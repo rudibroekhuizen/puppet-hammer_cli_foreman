b2765a40d6d0914fbbb4edf48b9d07ab841bf71c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-17 22:28:11 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,8 +2,17 @@
 #
 class hammer_cli_foreman::config {
 
+  # Create directories for config files
   file { ['/etc/hammer', '/etc/hammer/cli.modules.d']:
     ensure => 'directory',
   }
   
+  # Modify config file
+  augeas { "sshd_config":
+    context => "/files/etc/hammer/cli.modules.d/cli_config.yml",
+    changes => [
+      "set spec[user = 'joe']/user joe",
+      "set spec[user = 'joe']/host_group/host ALL",
+    ],
+  }
 }
