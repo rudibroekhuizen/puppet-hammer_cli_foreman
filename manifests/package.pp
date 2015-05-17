2bf4de726ed7bc8aa2ac4dba0af1a09654e9c4db
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-17 20:14:55 +0200

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,9 +2,16 @@
 #
 class hammer_cli_foreman::package {
 
+  # Install packages needed (dependencies)
+  package { ['ruby-dev', 'make']:
+    ensure => installed,
+  }
+
+  # Install hammer
   package { ['hammer_cli', 'hammer_cli_foreman']:
     ensure   => installed,
     provider => gem,
+    require  => Package ['ruby-dev', 'make'],
   }
   
 }
