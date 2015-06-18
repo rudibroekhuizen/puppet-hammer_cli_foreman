c0cabb124ded9a6c9f4b91d3e8d4cdd008bd025a
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-18 16:35:15 +0200

    Update foreman_config.pp

--- manifests/defines/foreman_config.pp
+++ manifests/defines/foreman_config.pp
@@ -1,3 +1,22 @@
 # == Define: hammer_cli_foreman::defines::foreman_config.pp
 #
 define hammer_cli_foreman::defines::foreman_config (
+  $setting,
+  $value,
+  ) {
+
+  # Common settings
+  Ini_setting {
+    ensure         => present,
+    path           => "/etc/hammer/cli.modules.d/${title}",
+    section        => 'foreman',
+    section_prefix => ":",
+    section_suffix => "",
+  }
+
+  ini_setting { "ini_${title}:
+    setting => $setting,
+    value   => $value,
+  }
+  
+}
